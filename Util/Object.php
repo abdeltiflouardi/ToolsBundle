5df8b8a0feec3f137b260affa5ca8a2a084e3df3
ouardisoft <louardi.abdeltif@yahoo.fr>
2013-09-11 22:16:36 +0100

    Add object copy util

--- Util/Object.php
+++ Util/Object.php
@@ -7,46 +7,51 @@ namespace OS\ToolsBundle\Util;
  */
 class Object
 {
-        private $from;
-        private $to;
 
-        public function setTo($to)
-        {
-                $this->to = $to;
+    private $from;
+    private $to;
+
+    public function setTo($to)
+    {
+        $this->to = $to;
+    }
+
+    public function setFrom($from)
+    {
+        $this->from = $from;
+    }
+
+    public function copy(array $mapper = array(), array $except = array())
+    {
+        $reflect = new \ReflectionClass($this->from);
+        $props   = $reflect->getProperties();
+
+        foreach ($props as $prop) {
+            $name = $prop->getName();
+            if (array_key_exists($name, $except)) {
+                continue;
+            }
+
+            $prop->setAccessible(true);
+            $value = $prop->getValue($this->from);
+
+            if (array_key_exists($name, $mapper)) {
+                $name = $mapper[$name];
+            }
+
+            $this->setPropertyValue($this->to, $name, $value);
         }
+    }
 
-        public function setFrom($from)
-        {
-                $this->from = $from;
+    public function setPropertyValue($obj, $propertyName, $propertyValue)
+    {
+        $reflect = new \ReflectionClass($obj);
+        if ($reflect->hasProperty($propertyName)) {
+            $prop = $reflect->getProperty($propertyName);
+
+            $prop->setAccessible(true);
+            $prop->setValue($obj, $propertyValue);
         }
-
-        public function copy(array $mapper = array())
-        {
-                $reflect = new ReflectionClass($this->from);
-                $props   = $reflect->getProperties();
-
-                foreach ($props as $prop) {
-                        $prop->setAccessible(true);
-                        $value = $prop->getValue($this->from) . "\n";
-
-                        $name = $prop->getName();
-                        if (array_key_exists($name, $mapper)) {
-                                $name = $mapper[$name];
-                        }
-
-                        $this->setPropertyValue($this->to, $name, $value);
-                }
-        }
-
-        public function setPropertyValue($obj, $propertyName, $propertyValue)
-        {
-                $reflect = new ReflectionClass($obj);
-                if ($reflect->hasProperty($propertyName)) {
-                        $prop   = $reflect->getProperty($propertyName);
-
-                        $prop->setAccessible(true);
-                        $prop->setValue($obj, $propertyValue);
-                }
-        }
+    }
 }
 
