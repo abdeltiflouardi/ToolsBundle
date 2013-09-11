0bd07160748dc7e216e9219521a1172625db483a
LOUARDI Abdeltif <louardi.abdeltif@07zr.com>
2013-09-11 16:48:31 +0100

    Add Object class

---
+++ Util/Object.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace OS\ToolsBundle\Util;
+
+/**
+ * @author ouardisoft
+ */
+class Object
+{
+        private $from;
+        private $to;
+
+        public function setTo($to)
+        {
+                $this->to = $to;
+        }
+
+        public function setFrom($from)
+        {
+                $this->from = $from;
+        }
+
+        public function copy(array $mapper = array())
+        {
+                $reflect = new ReflectionClass($this->from);
+                $props   = $reflect->getProperties();
+
+                foreach ($props as $prop) {
+                        $prop->setAccessible(true);
+                        $value = $prop->getValue($this->from) . "\n";
+
+                        $name = $prop->getName();
+                        if (array_key_exists($name, $mapper)) {
+                                $name = $mapper[$name];
+                        }
+
+                        $this->setPropertyValue($this->to, $name, $value);
+                }
+        }
+
+        public function setPropertyValue($obj, $propertyName, $propertyValue)
+        {
+                $reflect = new ReflectionClass($obj);
+                if ($reflect->hasProperty($propertyName)) {
+                        $prop   = $reflect->getProperty($propertyName);
+
+                        $prop->setAccessible(true);
+                        $prop->setValue($obj, $propertyValue);
+                }
+        }
+}
+
